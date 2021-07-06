788ddc0deee5753902547eac1fbc353eb6ffcf03
Tatsu Caleum <shinyakogami2010@gmail.com>
2021-07-06 23:35:52 +0700

    update

---
+++ WebApplication6/WebApplication6/Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication6.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Lien ket cua toi";
+            return View();
+        }
+        public String ChaoMung(String ten, int solan)
+        {
+            return HttpUtility.HtmlEncode("Xin chao "+ten+" So lan la"+solan);
+
+        }
+
+    }
+}
---
+++ WebApplication6/WebApplication6/Controllers/ThongTinSinhVienController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication6.Controllers
+{
+    public class ThongTinSinhVienController : Controller
+    {
+        // GET: ThongTinSinhVien
+        public ActionResult Index(string masv, String hoten, String ngaysinh, String quequan)
+        {
+           
+            return View();
+        }
+    }
+}
