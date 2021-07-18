24b6e9de6517233dbc574f8c4abb9d93aa5da55a
Tatsu Caleum <shinyakogami2010@gmail.com>
2021-07-18 21:03:21 +0700

    update

--- WebApplication7/WebApplication7/App_Start/RouteConfig.cs
+++ WebApplication7/WebApplication7/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebApplication7
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "TinhSo", action = "Register", id = UrlParameter.Optional }
+                defaults: new { controller = "StudentRegistation", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
